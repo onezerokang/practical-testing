86004af096c61f4eb383be33543bdb214db41c93
Wonyoung Kang <onezerokang@gmail.com>
2023-08-31 16:31:50 +0900

    Business Layer 테스트 (1)

---
+++ src/main/java/sample/cafekiosk/spring/domain/api/service/order/request/OrderCreateRequest.java
@@ -0,0 +1,16 @@
+package sample.cafekiosk.spring.domain.api.service.order.request;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class OrderCreateRequest {
+    private List<String> productNumbers;
+
+    @Builder
+    private OrderCreateRequest(List<String> productNumbers) {
+        this.productNumbers = productNumbers;
+    }
+}
