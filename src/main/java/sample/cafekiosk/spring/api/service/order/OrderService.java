37f6c5583b8e30ddf8dd155a8947d7023a42783a
Wonyoung Kang <onezerokang@gmail.com>
2024-03-03 13:50:45 +0900

    Business Layer 테스트(2)

--- src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -11,6 +11,8 @@ import sample.cafekiosk.spring.domain.product.ProductRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -22,11 +24,21 @@ public class OrderService {
     public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDatTime) {
         List<String> productNumbers = request.getProductNumbers();
 
-        List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+        List<Product> duplicateProducts = findProductsBy(productNumbers);
 
-        Order order = Order.create(products, registeredDatTime);
+        Order order = Order.create(duplicateProducts, registeredDatTime);
         Order savedOrder = orderRepository.save(order);
 
         return OrderResponse.of(savedOrder);
+    }
+
+    private List<Product> findProductsBy(List<String> productNumbers) {
+        List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+        Map<String, Product> productMap = products.stream()
+                .collect(Collectors.toMap(Product::getProductNumber, p -> p));
+
+        return productNumbers.stream()
+                .map(productMap::get)
+                .toList();
     }
 }
